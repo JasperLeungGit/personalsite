11e6c18164efa4f9b9602586a5e907073517d103
root <root@LAPTOP-PJF4SK28.localdomain>
2020-07-04 19:07:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSpring, animated, config } from "react-spring";
+import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 
-function App() {
+const App = () => {
+  const nameAnimation = useSpring({
+    from: { opacity: 0, transform: "translate3d(100%,0vh,0)" },
+    to: { opacity: 1, transform: "translate3d(0%,50vh,0)" },
+  });
+  const menuAnimation = useSpring({
+    from: { opacity: 0, transform: "translate3d(-100%,85vh,0)" },
+    to: { opacity: 1, transform: "translate3d(0%,85vh,0)" },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Helmet>
+        <style>{"body { background-color: black; overflow-x: hidden; }"}</style>
+      </Helmet>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto"
+      ></link>
+      <animated.div id="gif">
+        <img
+          className="gif"
+          src="http://5b0988e595225.cdn.sohucs.com/images/20180906/ac2ca14b0ee24ba1834430e25a384ae7.gif"
+        ></img>
+      </animated.div>
+      <animated.div style={nameAnimation} id="main">
+        <h1 className="nameText">Jasper Leung</h1>
+      </animated.div>
+      <animated.div style={menuAnimation} id="menu">
+        <h1 className="homeText">Home</h1>
+        <h1 className="aboutText">About</h1>
+        <h1 className="projectsText">Projects</h1>
+        <h1 className="contactText">Contact</h1>
+      </animated.div>
     </div>
   );
-}
+};
 
 export default App;
